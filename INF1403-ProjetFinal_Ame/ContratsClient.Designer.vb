4a39e3faed6718eeb15971f98185253381462aaa
Amélie Desgagnés <desa111@uqo.ca>
2021-03-09 18:36:31 -0500

    Intégration de la form dans une seule fenetre. On peut maintenant consulter, ajouter et modifier

--- INF1403-ProjetFinal_Ame/ContratsClient.Designer.vb
+++ INF1403-ProjetFinal_Ame/ContratsClient.Designer.vb
@@ -34,16 +34,19 @@ Partial Class ContratsClient
         Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Lb_Contrats = New System.Windows.Forms.Label()
         Me.Btn_NouveauContrat = New System.Windows.Forms.Button()
-        Me.Btn_Consulter = New System.Windows.Forms.Button()
         Me.Btn_Modifier = New System.Windows.Forms.Button()
         Me.Btn_Resilier = New System.Windows.Forms.Button()
-        DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.ColIdContrat = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.ColCodeClient = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.ColDateDebutContrat = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.ColDateFinContrat = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.LbIdContrat = New System.Windows.Forms.Label()
+        Me.LbDateFin = New System.Windows.Forms.Label()
+        Me.LbDateDebut = New System.Windows.Forms.Label()
+        Me.LbCodeClient = New System.Windows.Forms.Label()
+        Me.TxtBoxIdContrat = New System.Windows.Forms.TextBox()
+        Me.TxtBoxCodeClient = New System.Windows.Forms.TextBox()
+        Me.TxtBoxDateDebut = New System.Windows.Forms.TextBox()
+        Me.TxtBoxDateFin = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Mn_navig.SuspendLayout()
-        CType(DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ContextMenuStrip1
@@ -74,7 +77,7 @@ Partial Class ContratsClient
         Me.Mn_navig.Name = "Mn_navig"
         Me.Mn_navig.Padding = New System.Windows.Forms.Padding(9, 7, 9, 9)
         Me.Mn_navig.RightToLeft = System.Windows.Forms.RightToLeft.Yes
-        Me.Mn_navig.Size = New System.Drawing.Size(638, 40)
+        Me.Mn_navig.Size = New System.Drawing.Size(995, 40)
         Me.Mn_navig.TabIndex = 1
         Me.Mn_navig.Text = "MenuStrip1"
         '
@@ -128,79 +131,123 @@ Partial Class ContratsClient
         '
         'Btn_NouveauContrat
         '
-        Me.Btn_NouveauContrat.Location = New System.Drawing.Point(54, 120)
+        Me.Btn_NouveauContrat.Location = New System.Drawing.Point(470, 140)
         Me.Btn_NouveauContrat.Name = "Btn_NouveauContrat"
-        Me.Btn_NouveauContrat.Size = New System.Drawing.Size(81, 29)
+        Me.Btn_NouveauContrat.Size = New System.Drawing.Size(132, 40)
         Me.Btn_NouveauContrat.TabIndex = 3
         Me.Btn_NouveauContrat.Text = "Nouveau"
         Me.Btn_NouveauContrat.UseVisualStyleBackColor = True
         '
-        'Btn_Consulter
-        '
-        Me.Btn_Consulter.Location = New System.Drawing.Point(165, 120)
-        Me.Btn_Consulter.Name = "Btn_Consulter"
-        Me.Btn_Consulter.Size = New System.Drawing.Size(81, 29)
-        Me.Btn_Consulter.TabIndex = 5
-        Me.Btn_Consulter.Text = "Consulter"
-        Me.Btn_Consulter.UseVisualStyleBackColor = True
-        '
         'Btn_Modifier
         '
-        Me.Btn_Modifier.Location = New System.Drawing.Point(279, 120)
+        Me.Btn_Modifier.Location = New System.Drawing.Point(636, 140)
         Me.Btn_Modifier.Name = "Btn_Modifier"
-        Me.Btn_Modifier.Size = New System.Drawing.Size(81, 29)
+        Me.Btn_Modifier.Size = New System.Drawing.Size(129, 40)
         Me.Btn_Modifier.TabIndex = 6
         Me.Btn_Modifier.Text = "Modifier"
         Me.Btn_Modifier.UseVisualStyleBackColor = True
         '
         'Btn_Resilier
         '
-        Me.Btn_Resilier.Location = New System.Drawing.Point(395, 120)
+        Me.Btn_Resilier.Location = New System.Drawing.Point(798, 140)
         Me.Btn_Resilier.Name = "Btn_Resilier"
-        Me.Btn_Resilier.Size = New System.Drawing.Size(81, 29)
+        Me.Btn_Resilier.Size = New System.Drawing.Size(127, 40)
         Me.Btn_Resilier.TabIndex = 7
         Me.Btn_Resilier.Text = "Résilier"
         Me.Btn_Resilier.UseVisualStyleBackColor = True
         '
+        'LbIdContrat
+        '
+        Me.LbIdContrat.AutoSize = True
+        Me.LbIdContrat.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbIdContrat.Location = New System.Drawing.Point(59, 149)
+        Me.LbIdContrat.Name = "LbIdContrat"
+        Me.LbIdContrat.Size = New System.Drawing.Size(83, 20)
+        Me.LbIdContrat.TabIndex = 9
+        Me.LbIdContrat.Text = "ID Contrat"
+        '
+        'LbDateFin
+        '
+        Me.LbDateFin.AutoSize = True
+        Me.LbDateFin.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbDateFin.Location = New System.Drawing.Point(59, 373)
+        Me.LbDateFin.Name = "LbDateFin"
+        Me.LbDateFin.Size = New System.Drawing.Size(70, 20)
+        Me.LbDateFin.TabIndex = 10
+        Me.LbDateFin.Text = "Date Fin"
+        '
+        'LbDateDebut
+        '
+        Me.LbDateDebut.AutoSize = True
+        Me.LbDateDebut.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbDateDebut.Location = New System.Drawing.Point(59, 294)
+        Me.LbDateDebut.Name = "LbDateDebut"
+        Me.LbDateDebut.Size = New System.Drawing.Size(92, 20)
+        Me.LbDateDebut.TabIndex = 11
+        Me.LbDateDebut.Text = "Date Début"
+        '
+        'LbCodeClient
+        '
+        Me.LbCodeClient.AutoSize = True
+        Me.LbCodeClient.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbCodeClient.Location = New System.Drawing.Point(59, 221)
+        Me.LbCodeClient.Name = "LbCodeClient"
+        Me.LbCodeClient.Size = New System.Drawing.Size(91, 20)
+        Me.LbCodeClient.TabIndex = 12
+        Me.LbCodeClient.Text = "Code Client"
+        '
+        'TxtBoxIdContrat
+        '
+        Me.TxtBoxIdContrat.Location = New System.Drawing.Point(175, 151)
+        Me.TxtBoxIdContrat.Name = "TxtBoxIdContrat"
+        Me.TxtBoxIdContrat.Size = New System.Drawing.Size(191, 20)
+        Me.TxtBoxIdContrat.TabIndex = 13
+        '
+        'TxtBoxCodeClient
+        '
+        Me.TxtBoxCodeClient.Location = New System.Drawing.Point(175, 223)
+        Me.TxtBoxCodeClient.Name = "TxtBoxCodeClient"
+        Me.TxtBoxCodeClient.Size = New System.Drawing.Size(191, 20)
+        Me.TxtBoxCodeClient.TabIndex = 14
+        '
+        'TxtBoxDateDebut
+        '
+        Me.TxtBoxDateDebut.Location = New System.Drawing.Point(175, 294)
+        Me.TxtBoxDateDebut.Name = "TxtBoxDateDebut"
+        Me.TxtBoxDateDebut.Size = New System.Drawing.Size(191, 20)
+        Me.TxtBoxDateDebut.TabIndex = 15
+        '
+        'TxtBoxDateFin
+        '
+        Me.TxtBoxDateFin.Location = New System.Drawing.Point(175, 373)
+        Me.TxtBoxDateFin.Name = "TxtBoxDateFin"
+        Me.TxtBoxDateFin.Size = New System.Drawing.Size(191, 20)
+        Me.TxtBoxDateFin.TabIndex = 16
+        '
         'DataGridView1
         '
-        DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
-        DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColIdContrat, Me.ColCodeClient, Me.ColDateDebutContrat, Me.ColDateFinContrat})
-        DataGridView1.Location = New System.Drawing.Point(58, 188)
-        DataGridView1.Name = "DataGridView1"
-        DataGridView1.Size = New System.Drawing.Size(455, 158)
-        DataGridView1.TabIndex = 8
-        '
-        'ColIdContrat
-        '
-        Me.ColIdContrat.HeaderText = "ID Contrat"
-        Me.ColIdContrat.Name = "ColIdContrat"
-        '
-        'ColCodeClient
-        '
-        Me.ColCodeClient.HeaderText = "Code Client"
-        Me.ColCodeClient.Name = "ColCodeClient"
-        '
-        'ColDateDebutContrat
-        '
-        Me.ColDateDebutContrat.HeaderText = "Date Début"
-        Me.ColDateDebutContrat.Name = "ColDateDebutContrat"
-        '
-        'ColDateFinContrat
-        '
-        Me.ColDateFinContrat.HeaderText = "Date Fin"
-        Me.ColDateFinContrat.Name = "ColDateFinContrat"
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(461, 209)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(475, 209)
+        Me.DataGridView1.TabIndex = 17
         '
         'ContratsClient
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(638, 389)
-        Me.Controls.Add(DataGridView1)
+        Me.ClientSize = New System.Drawing.Size(995, 475)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TxtBoxDateFin)
+        Me.Controls.Add(Me.TxtBoxDateDebut)
+        Me.Controls.Add(Me.TxtBoxCodeClient)
+        Me.Controls.Add(Me.TxtBoxIdContrat)
+        Me.Controls.Add(Me.LbCodeClient)
+        Me.Controls.Add(Me.LbDateDebut)
+        Me.Controls.Add(Me.LbDateFin)
+        Me.Controls.Add(Me.LbIdContrat)
         Me.Controls.Add(Me.Btn_Resilier)
         Me.Controls.Add(Me.Btn_Modifier)
-        Me.Controls.Add(Me.Btn_Consulter)
         Me.Controls.Add(Me.Btn_NouveauContrat)
         Me.Controls.Add(Me.Lb_Contrats)
         Me.Controls.Add(Me.Mn_navig)
@@ -209,7 +256,7 @@ Partial Class ContratsClient
         Me.Text = "Form1"
         Me.Mn_navig.ResumeLayout(False)
         Me.Mn_navig.PerformLayout()
-        CType(DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -226,12 +273,15 @@ Partial Class ContratsClient
     Friend WithEvents MnItem_MonProfil As ToolStripMenuItem
     Friend WithEvents Lb_Contrats As Label
     Friend WithEvents Btn_NouveauContrat As Button
-    Friend WithEvents Btn_Consulter As Button
     Friend WithEvents Btn_Modifier As Button
     Friend WithEvents Btn_Resilier As Button
-    Public Shared WithEvents DataGridView1 As DataGridView
-    Friend WithEvents ColIdContrat As DataGridViewTextBoxColumn
-    Friend WithEvents ColCodeClient As DataGridViewTextBoxColumn
-    Friend WithEvents ColDateDebutContrat As DataGridViewTextBoxColumn
-    Friend WithEvents ColDateFinContrat As DataGridViewTextBoxColumn
+    Friend WithEvents LbIdContrat As Label
+    Friend WithEvents LbDateFin As Label
+    Friend WithEvents LbDateDebut As Label
+    Friend WithEvents LbCodeClient As Label
+    Friend WithEvents TxtBoxIdContrat As TextBox
+    Friend WithEvents TxtBoxCodeClient As TextBox
+    Friend WithEvents TxtBoxDateDebut As TextBox
+    Friend WithEvents TxtBoxDateFin As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
 End Class
--- INF1403-ProjetFinal_Ame/NouveauContrat.vb
+++ INF1403-ProjetFinal_Ame/NouveauContrat.vb
@@ -1,8 +1,35 @@
 ﻿Imports System.ComponentModel
 
 Public Class NouveauContrat
+    Private IdClient As String
+    Dim CodeClient As String
+    Dim DateDebut As String
+    Dim DateFin As String
+
+    Public Sub New()
+    End Sub
+
+    Public Sub New(idClient As String, codeClient As String, dateDebut As String, dateFin As String)
+        If String.IsNullOrWhiteSpace(idClient) Then
+            Throw New ArgumentException($"'{NameOf(idClient)}' ne peut pas être null ou un espace", NameOf(idClient))
+        End If
+
+        Me.IdClient = idClient
+        Me.CodeClient = codeClient
+        Me.DateDebut = dateDebut
+        Me.DateFin = dateFin
+    End Sub
+    Private Sub NouveauContrat_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
     Private Sub BtnAjouter_Click(sender As Object, e As EventArgs) Handles BtnAjouter.Click
         ContratsClient.DataGridView1.Rows.Add(TxtBoxIdContrat.Text, TxtBoxCodeClient.Text, TxtBoxDateDebut.Text, TxtBoxDateFin.Text)
+
+        'Vide les zones de text'
+        TxtBoxIdContrat.Text = ""
+        TxtBoxCodeClient.Text = ""
+        TxtBoxDateDebut.Text = ""
+        TxtBoxDateFin.Text = ""
 
     End Sub
 End Class
